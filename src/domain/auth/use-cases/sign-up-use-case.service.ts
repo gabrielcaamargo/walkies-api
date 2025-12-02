f1f4cf28521335754a6835a4857f4bf935d14150
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-12-01 23:39:20 -0300

    feat: update use case contract

--- src/domain/auth/use-cases/sign-up-use-case.service.ts
+++ src/domain/auth/use-cases/sign-up-use-case.service.ts
@@ -1,4 +1,16 @@
 import {Injectable} from "@nestjs/common";
+import type {AuthRepository} from "../auth.repository";
+import {SignupResponse, UseCaseContract} from "@shared";
+import {SignupDto} from "../dto/signup.dto";
+import {Role} from "@domain";
 
 @Injectable()
-export class SignUpUseCaseService {}
+export class SignUpUseCaseService implements UseCaseContract<SignupDto, SignupResponse> {
+	constructor(private readonly authRepository: AuthRepository) {}
+
+	async execute(input: SignupDto, role: Role): Promise<SignupResponse> {
+		const user = await this.authRepository.signUp(input, role);
+
+		return user;
+	}
+}
